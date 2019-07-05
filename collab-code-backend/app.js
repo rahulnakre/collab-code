6d5305a956a1a5dba16b205cce02253d35ed0b9f
nakrerah <rahul.nakre@mail.utoronto.ca>
2019-07-05 09:41:37 +0530

    added backend

---
+++ collab-code-backend/app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const socketIo = require("socket.io");
+const axios = require("axios");
+
+const port = process.env.PORT || 4001;
+const index = require("./routes/index");
+
+app.use(index);
+const server = http.createServer(app);
+//io is a Socket.IO server instance attached to an instance of 
+//http.Server listening for incoming events (.listen() used below)
+const io = socketIo(server);
+
+let interval;
+var i = 0;	
+var clients = 0;	
+io.on("connection", socket => {
+	clients++;
+	console.log("new client connected");
+	io.sockets.emit('broadcast', { description: clients + ' clients connected!'});
+	//socket.emit("socketInfo", "");
+	/*interval = setInterval( () => {
+		socket.emit("sent-from-server", `emitting rn: ${i}`);
+		i++;
+	}, 2500);*/
+
+	socket.on("sent-from-client", data => {
+		socket.broadcast.emit('peer-message', { peer: "peer typed: " + JSON.stringify(data) });
+		//io.emit('peer-message', `peer: ${data}`);
+		console.log(data);
+	});
+
+	socket.on("disconnect", () => {
+		clients--;
+		console.log("client disconnected");
+		io.sockets.emit("broadcast", { description: clients + " clients connected"})
+		clearInterval(interval);
+	});
+});
+
+/*io.on("updatedTextModelState", socket => {
+	console.log("here i am");
+
+});*/
+
+server.listen(port, () => console.log(`listening on port ${port}`))
