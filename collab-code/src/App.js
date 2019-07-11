9227d403cea06b566f335d601a7335225ab067d6
rahul <rahulnakre@gmail.com>
2019-07-11 02:46:41 -0400

    merged the delete and add method into one, cleaned up some console.logs

--- collab-code/src/App.js
+++ collab-code/src/App.js
@@ -35,12 +35,9 @@ class App extends React.Component {
 			socket: socket
 		})
 		socket.on(SENT_FROM_SERVER, (data) => { // for periodic pings
-			var arr = data.split(" ")
-			console.log(arr[arr.length-1])
+			// nothing to see here yet
 		})
 		socket.on(PEER_MESSAGE, data => { // when peer sends a message
-			console.log(data)
-			console.log("received peer emit")
 			// we got peer's recently typed text and its abs pos
 			// so let's place that change locally
 			// TODO: upgrade from naive abs pos way to crdt
@@ -50,10 +47,7 @@ class App extends React.Component {
 			if(data.origin === "+delete") {
 				//this.deletePeerTextAtAbsPos(data.absPos.line, data.absPos.ch)
 				this.mergeWithPeerTextAtAbsPos("", data.from, data.to)
-
-				console.log("deleted")
 			} else if (data.origin === "cut") {
-				console.log("cutted")
 			} else {
 				// his deals with typed input and paste
 				this.mergeWithPeerTextAtAbsPos(data.text, data.from, data.to)
@@ -70,17 +64,14 @@ class App extends React.Component {
 	}
 
 	handleBeforeTextModelChange = (editor, data, value) => {
-		console.log(data)
 		if (data.text === "") {
-			console.log(data.origin)
+			// console.log(data.origin)
 		}
 		this.setState({
 			textModel: value 
 		})
 		// we only want to emit if this client typed or pasted something
-		console.log(this.state.receivedFromPeer)
 		if (!this.state.receivedFromPeer) {
-			console.log("gona emit to peer")
 			this.state.socket.emit(SENT_FROM_CLIENT, 
 				{ 
 					text: data.text,
@@ -94,22 +85,13 @@ class App extends React.Component {
 		this.setState({
 			receivedFromPeer: false
 		})
-		//console.log(data)
 	}
 
-
-	/*deletePeerTextAtAbsPos = (line, ch) => { //could merge this with addPeerTextAtAbsPos
-		this.editorInstance.replaceRange("", {line: line, ch: ch}, {line: line, ch: ch+1})
-	}*/
 
 	mergeWithPeerTextAtAbsPos = (peerText, from, to) => {
 		/* The text the peer sent will be added locally at the same absolute position
 		   as the peer.
 		*/
-		//console.log(peerText)
-		//console.log(from)
-		//console.log(ch)
-		//this.editorInstance.replaceRange(peerText, {line: line, ch: ch-1})
 		const fromLine = from.line
 		const fromCh = from.ch
 		const toLine = to.line
