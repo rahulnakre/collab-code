73cd490d7549f3aab985a6aae36d029b52fd3d21
nakrerah <rahul.nakre@mail.utoronto.ca>
2019-08-13 22:21:16 -0400

    somenewline handling, edits to findBefore, and inserting into right spot into CRDT

--- collab-code-backend/crdt/crdt.js
+++ collab-code-backend/crdt/crdt.js
@@ -36,15 +36,16 @@ class CRDT {
 		// Todo: DO I USE FROM OR TO?? or neither nvm
 		//console.log(from)
 		var char = new Char(val[0], siteId, from)
-		var checkIfNewLine = false
+		this.checkIfCurrCharIsAfterNewline(from.line, from.ch)
 
 		console.log("local insert...")
 		console.log("val: ", val)
 		if (val.length == 2 && val[0] == "" && val[1] == "") { 
-			checkIfNewLine = true
 			char = new Char("\n", siteId, from)
-
+			// only time we have to possibly remove a newline is when we're adding one
+			this.removeNewLine(from.line)
 		}
+
 		if (type === "+input") {
 			this.insertToCharArray(char, from)
 		}
@@ -59,16 +60,20 @@ class CRDT {
 			this.currLine = pos.line 
 		}
 
-		this.findPreviousPos(pos)
-		this.findNextPos(pos)
+		var prevPos = this.findPreviousPos(pos)
+		// console.log("prev pos... " + JSON.stringify(prevPos))
+		var nextPos = this.findNextPos(pos)
+		console.log("next pos... " + JSON.stringify(nextPos))
 
-		this.charArray[pos.line].push(char)
+
+		//this.charArray[pos.line].push(char)
+		this.charArray[pos.line].splice(pos.ch ,0, char)
 		//console.log(this.charArray[pos.line])
 	}
 
 	findPreviousPos(currentPos) {
 		//var prev = this.charArray[]
-		console.log("finding prev pos of...\n" + JSON.stringify(currentPos))
+		// console.log("finding prev pos of...\n" + JSON.stringify(currentPos))
 		// var prev = ""
 		if (currentPos.ch === 0) {
 			if (currentPos.line === 0) {
@@ -84,13 +89,16 @@ class CRDT {
 	}
 
 	findNextPos(currentPos) {
-		//console.log("finding next pos...\n" + JSON.stringify(currentPos))
+		console.log("finding next pos...\n" + JSON.stringify(currentPos))
 		const numLines = this.charArray.length
 		const numCharsOnLine = (this.charArray[currentPos.line] && this.charArray[currentPos.line].length) + 1 || 0
 		//console.log("num lines: ", numLines)
-		//console.log("chars on line ", numCharsOnLine)
-		if (currentPos.line === numLines - 1) {
+		console.log("chars on line ", numCharsOnLine)
+		if (currentPos.line === numLines - 1) { // last line
 			console.log("last")
+			return []
+		} else {
+
 		}
 	}
 
@@ -98,6 +106,17 @@ class CRDT {
 		//
 	}
 
+	checkIfCurrCharIsAfterNewline(line, ch) {
+
+	}
+
+	removeNewLine(line) {
+		/* checks if newline exists in a given line, and if it does,
+			it is removed	
+		*/
+		// console.log("check this line to remove newline: ", line)
+
+	}
 	
 	
 }
